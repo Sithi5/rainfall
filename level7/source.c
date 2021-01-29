23be070430f5bb1fe88d897c78e17823acc2f8b0
ChokMania <judumay@student.42.fr>
2021-01-29 12:24:46 +0100

    Push level7

--- level7/source.c
+++ level7/source.c
@@ -1,43 +1,33 @@
-char *c;
+#include <stdio.h>
+#include <stdlib.h>
+
+char *c[78];
 
 void	m(void)
 {
 	print("%s - %d\n", c, time(0));
-	return
+	return;
 }
 
-int		main(int ac, char **av):
+int		main(int ac, char **av)
 {
-	malloc(8);
-	malloc(8);
-	malloc(8);
-	malloc(8);
-	fgets(c, 68, fopen("/home/user/level8/.pass", "r"));
+	char *str1;
+	void *str2;
+	char *str3;
+	FILE *file;
+
+	str1 = (char *)malloc(8);
+	*str1 = 1;
+	str2 = (void *)malloc(8);
+	str1[1] = str2;
+	str3 = (char *)malloc(8);
+	*str3 = 2;
+	str2 = (void *)malloc(8);
+	str3[1] = str2;
+	strcpy(str1[1], av[1]);
+	strcpy(str3[1], av[2]);
+	file = fopen("/home/user/level8/.pass", "r");
+	fgets(c, 68, file);
 	puts("~~");
 	return(0);
 }
-
-
-// undefined4 main(undefined4 param_1,int param_2)
-
-// {
-//   undefined4 *puVar1;
-//   void *pvVar2;
-//   undefined4 *puVar3;
-//   FILE *__stream;
-  
-//   puVar1 = (undefined4 *)malloc(8);
-//   *puVar1 = 1;
-//   pvVar2 = malloc(8);
-//   puVar1[1] = pvVar2;
-//   puVar3 = (undefined4 *)malloc(8);
-//   *puVar3 = 2;
-//   pvVar2 = malloc(8);
-//   puVar3[1] = pvVar2;
-//   strcpy((char *)puVar1[1],*(char **)(param_2 + 4));
-//   strcpy((char *)puVar3[1],*(char **)(param_2 + 8));
-//   __stream = fopen("/home/user/level8/.pass","r");
-//   fgets(c,0x44,__stream);
-//   puts("~~");
-//   return 0;
-// }
