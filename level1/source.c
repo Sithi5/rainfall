d28a76c55f4a20da59118c6c4d0cca013d6c32b3
Sithi5 <ma.sithis@gmail.com>
2021-02-04 19:50:27 +0100

    better source

--- level1/source.c
+++ level1/source.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-void	run(void)
+void run(void)
 {
-	fwrite("Good... Wait what?", 1, 19, stdout);
-	system("bin/sh");
+    fwrite("Good... Wait what?\n", 1, 19, stdout);
+    system("/bin/sh");
+    return;
 }
 
-int		main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	char* str[64];
-	gets(str);
-	return(0);
+    char str[64];
+    gets(str);
+    return (0);
 }
